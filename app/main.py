80c36aa08cc470ae57239a258a902e2a3d7213cb
bunkerwallx <39147256+Bunkerwallx@users.noreply.github.com>
2024-07-10 01:34:15 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -101,24 +101,8 @@ def tracking_pixel():
 
 if __name__ == '__main__':
     app.run()#!/usr/bin/env python
-## 
-##
-##
-#
-#
-#  Se cargan las bibliotecas python
-
-from flask import requests, send_file, about, url_for
-import loggin
-from datetime import datetime
-import os
-import smtplib
-from email.mime.text import MIMETEXT
-from twilio.rest import Client
-from app import app
 
 
-# Se realiza la configuracion de logger
 
-log_filename = os.getenv('LOG_FILENAME', 'logs/trackimg.log')
-logging.basicConfig(filename=log_filename, level=logging.INFO, format)
+
+
